0f4ec6e81397d0d3b5359308738c0245ebccaf78
Woon Jeng <woonjeng@gmail.com>
2021-09-15 14:22:48 +0800

    initial commit on flashflood algorithm that is used to detect if board pieces are capbured

--- go.py
+++ go.py
@@ -114,8 +114,8 @@ def equal_matrices(board1, board2):
                 return False
     return True
 
-def flashfood(board):
-    #the flashfood algorithm is capable of detecting that if a capture has happened or not
+def flashflood(board):
+    #the flashflood algorithm is capable of detecting that if a capture has happened or not
 
 
 if __name__ == "__main__":
